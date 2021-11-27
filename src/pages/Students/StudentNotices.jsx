fd01eb21ee819ddf1a1ee68ffa1bd7e067259ee1
zubayerhimel <zhimel007@gmail.com>
2021-11-28 00:42:06 +0600

    feat: added feedback and notice page

--- src/pages/Students/StudentNotices.jsx
+++ src/pages/Students/StudentNotices.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Alert, Row, Typography } from "antd";
 
-const StudentNotices = () => {
+const { Title } = Typography;
+
+const TutorNotices = () => {
   return (
     <div>
-      <h1>notices</h1>
+      <Row justify="center">
+        <Title level={2}>All Notices</Title>
+      </Row>
+      <Alert
+        message={
+          <div>
+            <Row justify="space-between">
+              <Title level={4}>This is your first warning</Title>
+              <span>21 November, 2021</span>
+            </Row>
+          </div>
+        }
+        description="Detailed description and advice about successful copywriting."
+        type="success"
+        showIcon
+      />
     </div>
   );
 };
 
-export default StudentNotices;
+export default TutorNotices;
