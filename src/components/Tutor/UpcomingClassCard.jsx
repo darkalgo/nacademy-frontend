20248a2481020845c13b8f63a5d4caec75a09159
zubayerhimel <zhimel007@gmail.com>
2021-11-27 14:01:45 +0600

    feat: added upcoming classes page component

---
+++ src/components/Tutor/UpcomingClassCard.jsx
@@ -0,0 +1,68 @@
+import { Button, Card, Col, Row, Typography } from "antd";
+import { CalendarFilled } from "@ant-design/icons";
+
+const { Title } = Typography;
+
+const UpcomingClassCard = () => {
+  return (
+    <Card className="card">
+      <Row gutter={[8, 8]}>
+        <Col xs={{ span: 8 }}>
+          <Title level={5} className="primary-color">
+            Class Name:
+          </Title>
+        </Col>
+        <Col xs={{ span: 16 }}>
+          <Title level={5}>Biology class 2nd chapter </Title>
+        </Col>
+        <Col xs={{ span: 8 }}>
+          <Title level={5} className="primary-color">
+            Date:
+          </Title>
+        </Col>
+        <Col xs={{ span: 16 }}>
+          <Title level={5}>November 21, 2021 </Title>
+        </Col>
+        <Col xs={{ span: 8 }}>
+          <Title level={5} className="primary-color">
+            Start Time:
+          </Title>
+        </Col>
+        <Col xs={{ span: 16 }}>
+          <Title level={5}>10:00 am </Title>
+        </Col>
+        <Col xs={{ span: 8 }}>
+          <Title level={5} className="primary-color">
+            End Time:
+          </Title>
+        </Col>
+        <Col xs={{ span: 16 }}>
+          <Title level={5}>11:00 am </Title>
+        </Col>
+        <Col xs={{ span: 8 }}>
+          <Title level={5} className="primary-color">
+            Student Name:
+          </Title>
+        </Col>
+        <Col xs={{ span: 16 }}>
+          <Title level={5}>Md. Kamal Ahmed </Title>
+        </Col>
+        <Col xs={{ span: 8 }}>
+          <Title level={5} className="primary-color">
+            Subject Name:
+          </Title>
+        </Col>
+        <Col xs={{ span: 16 }}>
+          <Title level={5}>Biology </Title>
+        </Col>
+        <Col xs={{ span: 24 }}>
+          <Button block type="primary" ghost icon={<CalendarFilled />}>
+            Reschedule Class Time
+          </Button>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default UpcomingClassCard;
--- src/pages/Tutors/TutorUpcomingClasses.jsx
+++ src/pages/Tutors/TutorUpcomingClasses.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Col, Row, Typography } from "antd";
+
+import UpcomingClassCard from "../../components/Tutor/UpcomingClassCard";
+
+const { Title } = Typography;
 
 const TutorUpcomingClasses = () => {
   return (
     <div>
-      <h1>Upcoming classes</h1>
+      <Row justify="center">
+        <Title level={2}>List Of Upcoming Classes</Title>
+      </Row>
+      <Row gutter={[16, 16]}>
+        <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
+          <UpcomingClassCard />
+        </Col>
+      </Row>
     </div>
   );
 };
