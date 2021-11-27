4b77a606cf54b620a01d637973dbbb61472366bc
zubayerhimel <zhimel007@gmail.com>
2021-11-27 15:57:20 +0600

    feat: added notice board page for tutor

--- src/pages/Tutors/TutorNotices.jsx
+++ src/pages/Tutors/TutorNotices.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Alert, Row, Typography } from "antd";
+
+const { Title } = Typography;
 
 const TutorNotices = () => {
   return (
     <div>
-      <h1>Notices</h1>
+      <Row justify="center">
+        <Title level={2}>All Notices</Title>
+      </Row>
+      <Alert
+        message={
+          <div>
+            <Row justify="space-between">
+              <Title level={4}>This is your first warning</Title>
+              <span>21 November, 2021</span>
+            </Row>
+          </div>
+        }
+        description="Detailed description and advice about successful copywriting."
+        type="success"
+        showIcon
+      />
     </div>
   );
 };
