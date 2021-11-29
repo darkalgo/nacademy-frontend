aedf841b52ca75bf2bf1d2072865d8cb3de08794
zubayerhimel <zhimel007@gmail.com>
2021-11-29 20:42:13 +0600

    feat: added withdrawal transaction page

---
+++ src/components/Admin/WithdrawalTransactionTable.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Table } from "antd";
+
+const WithdrawalTransactionTable = () => {
+  const columns = [
+    {
+      title: "Teacher Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Phone Number",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Institution Name",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Paid Amount",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Payment Method",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Account Number (Last 4 digit)",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Date",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
+  return <Table columns={columns} dataSource={data} scroll={{ x: 1000 }} />;
+};
+
+export default WithdrawalTransactionTable;
--- src/pages/Admin/AdminViewTransactions.jsx
+++ src/pages/Admin/AdminViewTransactions.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Col, Input, Row, Typography } from "antd";
+import WithdrawalTransactionTable from "../../components/Admin/WithdrawalTransactionTable";
+
+const { Search } = Input;
+const { Title } = Typography;
 
 const AdminViewTransactions = () => {
   return (
     <div>
-      <h1>transactions</h1>
+      <div className="center">
+        <Title level={2}></Title>
+      </div>
+
+      <Row justify="end" className="mb-2">
+        <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
+          <Search placeholder="Search text" enterButton />
+        </Col>
+      </Row>
+
+      <WithdrawalTransactionTable />
     </div>
   );
 };
