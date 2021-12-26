57af3262f8d795630ef23106b63ee6ccc6c590b1
zubayerhimel <zhimel007@gmail.com>
2021-12-26 21:14:38 +0600

    feat: added mark all to read functionality in student notice

--- src/pages/Students/StudentNotices.jsx
+++ src/pages/Students/StudentNotices.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Alert, List, Row, Spin, Typography } from "antd";
+import { Alert, Button, List, Row, Spin, Typography } from "antd";
 import { useHistory } from "react-router-dom";
 
 import { BaseAPI } from "../../utils/Api";
@@ -17,38 +17,74 @@ const TutorNotices = () => {
   const [noticeList, setNoticeList] = useState([]);
 
   useEffect(() => {
-    (async () => {
-      setLoading(true);
-      await BaseAPI.post(
-        "/students/notices",
-        {},
-        {
-          headers: {
-            Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
-          },
-        }
-      )
-        .then((res) => {
-          setNoticeList(res.data.data);
-        })
-        .catch((err) => {
-          if (err?.response?.data?.message) {
-            ErrorHandler(err?.response?.data?.message, history);
-          } else {
-            Notification("Something went wrong", "Please check your internet connection and try again or communicate with the admin", "error");
-          }
-        })
-        .finally(() => setLoading(false));
-    })();
+    getNotices();
     return () => {
       setNoticeList([]);
     };
   }, [history]);
 
+  // functions
+  const getNotices = async () => {
+    setLoading(true);
+    await BaseAPI.post(
+      "/students/notices",
+      {},
+      {
+        headers: {
+          Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+        },
+      }
+    )
+      .then((res) => {
+        console.log(res.data.data);
+        setNoticeList(res.data.data);
+      })
+      .catch((err) => {
+        if (err?.response?.data?.message) {
+          ErrorHandler(err?.response?.data?.message, history);
+        } else {
+          Notification("Something went wrong", "Please check your internet connection and try again or communicate with the admin", "error");
+        }
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const markAllToRead = async () => {
+    setLoading(true);
+    let unSeenNoticeIds = [];
+    const unSeenNotices = noticeList.filter((el) => el.is_view === 0);
+    unSeenNoticeIds.push(unSeenNotices.map((el) => el.id));
+    await BaseAPI.patch(
+      "/students/read-notices",
+      { notices: unSeenNoticeIds[0] },
+      {
+        headers: {
+          Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+        },
+      }
+    )
+      .then((res) => {
+        console.log(res);
+        getNotices();
+      })
+      .catch((err) => {
+        if (err?.response?.data?.message) {
+          ErrorHandler(err?.response?.data?.message, history);
+        } else {
+          Notification("Something went wrong", "Please check your internet connection and try again or communicate with the admin", "error");
+        }
+      })
+      .finally(() => setLoading(false));
+  };
+
   return (
     <Spin spinning={loading}>
       <Row justify="center">
         <Title level={2}>All Notices</Title>
+      </Row>
+
+      <Row justify="end">
+        <Button onClick={markAllToRead}>Mark All To Read</Button>
       </Row>
 
       {noticeList.length > 0 ? (
@@ -71,8 +107,9 @@ const TutorNotices = () => {
                   </div>
                 }
                 description={el.body}
-                type={el.type}
-                showIcon
+                type={el.is_view === 0 && el.type}
+                showIcon={el.is_view === 0 && true}
+                style={{ backgroundColor: `${el.is_view === 1 ? "#D4D4D4" : ""}` }}
               />
             </List.Item>
           )}
