298be68f0586222aa21e3a870de15b38ac85bc67
zubayerhimel <zhimel007@gmail.com>
2022-01-26 21:59:26 +0600

    feat: implemented student completed class page

--- src/pages/Students/StudentCompletedClasses.jsx
+++ src/pages/Students/StudentCompletedClasses.jsx
@@ -1,30 +1,113 @@
-import React from "react";
-import { Input, Table, Row, Typography, Col, Card } from "antd";
+import React, { useEffect, useState } from "react";
+import { Input, Table, Row, Typography, Col, Card, Spin, Select } from "antd";
+import { useHistory } from "react-router-dom";
+import FuzzySearch from "fuzzy-search";
+
+import { BaseAPI } from "../../utils/Api";
+import ErrorHandler from "../../components/controls/ErrorHandler";
+import Notification from "../../components/controls/Notification";
 
 const { Title } = Typography;
 const { Search } = Input;
+const { Option } = Select;
 
 const StudentCompletedClasses = () => {
+  const history = useHistory();
+
+  const [loading, setLoading] = useState(false);
+  const [classList, setClassList] = useState([]);
+  const [searchedClassList, setSearchedClassList] = useState([]);
+  const [status, setStatus] = useState("completed");
+
+  useEffect(() => {
+    getClassList(status);
+  }, []);
+
+  const getClassList = (status) => {
+    (async () => {
+      setLoading(true);
+      await BaseAPI.post(
+        "/students/classes",
+        { status },
+        {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+          },
+        }
+      )
+        .then((res) => {
+          console.log(res.data.data);
+          const info = res.data.data.map((el) => ({
+            ...el,
+            key: el.id,
+          }));
+          setClassList(info);
+          setSearchedClassList(info);
+        })
+        .catch((err) => {
+          if (err?.response?.data?.message) {
+            ErrorHandler(err?.response?.data?.message, history);
+          } else {
+            Notification("Something went wrong", "Please check your internet connection and try again or communicate with the admin", "error");
+          }
+        })
+        .finally(() => setLoading(false));
+    })();
+  };
+
+  const onClassStatusChange = (value) => {
+    if (value === "completed") {
+      setStatus("completed");
+    } else {
+      setStatus("cancel");
+    }
+    getClassList(value);
+  };
+
+  const searcher = new FuzzySearch(classList, ["agenda", "subject", "tutor_name"], { sort: true });
+
+  const handleOnSearch = (value) => {
+    if (value) {
+      const result = searcher.search(value);
+      setSearchedClassList([...result]);
+    } else {
+      setSearchedClassList(classList);
+    }
+  };
+  const handleSearchChange = (e) => {
+    if (e.target.value) {
+      const result = searcher.search(e.target.value);
+      setSearchedClassList([...result]);
+    } else {
+      setSearchedClassList(classList);
+    }
+  };
+
   const columns = [
     {
       title: "Class Name",
-      dataIndex: "name",
-      key: "name",
+      dataIndex: "agenda",
+      key: "agenda",
+    },
+    {
+      title: "Subject Name",
+      dataIndex: "subject",
+      key: "subject",
     },
     {
       title: "Date",
-      dataIndex: "age",
-      key: "age",
+      dataIndex: "date",
+      key: "date",
     },
     {
       title: "Start Time",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "start_time",
+      key: "start_time",
     },
     {
       title: "End Time",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "end_time",
+      key: "end_time",
     },
     {
       title: "Package Type",
@@ -33,13 +116,13 @@ const StudentCompletedClasses = () => {
     },
     {
       title: "Teacher Name",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "tutor_name",
+      key: "tutor_name",
     },
     {
       title: "Rating",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "tutor_ratings",
+      key: "tutor_ratings",
     },
     {
       title: "Action",
@@ -48,45 +131,28 @@ const StudentCompletedClasses = () => {
     },
   ];
 
-  const data = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: 32,
-      address: "New York No. 1 Lake Park",
-      tags: ["nice", "developer"],
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-      address: "London No. 1 Lake Park",
-      tags: ["loser"],
-    },
-    {
-      key: "3",
-      name: "Joe Black",
-      age: 32,
-      address: "Sidney No. 1 Lake Park",
-      tags: ["cool", "teacher"],
-    },
-  ];
-
-  const onSearch = (value) => console.log(value);
   return (
-    <>
+    <Spin spinning={loading}>
       <Row justify="center">
-        <Title level={2}>Completed Classes</Title>
+        <Title level={2}> {status === "completed" ? "Completed" : "Cancelled"} Classes</Title>
       </Row>
-      <Row justify="end" className="mb-2">
-        <Col xs={{ span: 24 }} md={{ span: 12 }} lg={{ span: 8 }}>
-          <Search placeholder="Search text" allowClear enterButton onSearch={onSearch} />
+
+      <Row justify="space-between" className="mb-1" gutter={[8, 8]}>
+        <Col xs={{ span: 24 }} md={{ span: 8 }} lg={{ span: 6 }}>
+          <Select defaultValue="completed" placeholder="Select Class Status" style={{ width: "100%" }} onChange={onClassStatusChange}>
+            <Option value="completed">Completed Class</Option>
+            <Option value="cancel">Cancelled Class</Option>
+          </Select>
+        </Col>
+        <Col xs={{ span: 24 }} md={{ span: 8 }} lg={{ span: 6 }}>
+          <Search placeholder="Search class" allowClear enterButton onSearch={handleOnSearch} onChange={handleSearchChange} />
         </Col>
       </Row>
+
       <Card className="card">
-        <Table columns={columns} dataSource={data} />
+        <Table columns={columns} dataSource={searchedClassList} scroll={{ x: 1000 }} />
       </Card>
-    </>
+    </Spin>
   );
 };
 
